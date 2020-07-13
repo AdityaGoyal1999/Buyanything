492754e41dd6863485e63b0ace0d1d6329f7bdd5
Aditya Goyal <adityagoyaldoon@gmail.com>
2020-07-13 11:57:25 +0530

    Creating the project

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/adityagoyal/Projects/python/Django/Youtube Crash Course/buyanything/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
